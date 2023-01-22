f95009d80d39d0a05bb002d0cbd67b57bf9d4208
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2023-01-22 11:53:57 -0400

    [CREATE] Add colors

---
+++ Movve/Support/Tokens/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  Movve
+//
+//  Created by Vinicius Galhardo Machado on 22/01/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static var primary: Color { Color("primary") }
+    static var secondary: Color = Color("secondary")
+    static var text: Color = Color("texts")
+}

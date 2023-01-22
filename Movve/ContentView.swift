73e9de4a3fa6875345e7975a0fbf57a02d605387
Vinicius Galhardo Machado <viniciusgm45@gmail.com>
2023-01-22 11:46:24 -0400

    [CREATE] First commit

---
+++ Movve/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Movve
+//
+//  Created by Vinicius Galhardo Machado on 22/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Movve/MovveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovveApp.swift
+//  Movve
+//
+//  Created by Vinicius Galhardo Machado on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MovveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
